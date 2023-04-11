fb9734ba9c6b2af9a395ba0a36a12e3822ac97b0
SYEDHASEENA <102157928+SYEDHASEENA@users.noreply.github.com>
2023-04-11 21:43:32 +0530

    new

--- nqueens.c
+++ nqueens.c
@@ -1,51 +1,66 @@
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
-int a[30],count=0;
-int place(int pos) {
-	int i;
-	for (i=1;i<pos;i++) {
-		if((a[i]==a[pos])||((abs(a[i]-a[pos])==abs(i-pos))))
-		   return 0;
-	}
-	return 1;
-}
-void print_sol(int n) {
-	int i,j;
-	count++;
-	printf("\n\nSolution #%d:\n",count);
-	for (i=1;i<=n;i++) {
-		for (j=1;j<=n;j++) {
-			if(a[i]==j)
-			    printf("Q\t"); else
-			    printf("*\t");
-		}
-		printf("\n");
-	}
-	}
-void queen(int n) {
-	int k=1;
-	a[k]=0;
-	while(k!=0) {
-		a[k]=a[k]+1;
-		while((a[k]<=n)&&!place(k))
-		   a[k]++;
-		if(a[k]<=n) {
-			if(k==n)
-			    print_sol(n); else {
-				k++;
-				a[k]=0;
-			}
-		} else
-		   k--;
-	}
-}
-void main() {
-	int i,n;
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-	printf("Enter the number of Queens\n");
-	scanf("%d",&n);
-	queen(n);
-	printf("\nTotal solutions=%d",count);
-	getch();
+int num_solutions = 0;
+
+
+void print_solution(int board[], int n);
+void solve(int board[], int col, int n);
+bool is_safe(int board[], int row, int col, int n);
+
+int main() {
+    int n;
+    printf("Enter the number of queens: ");
+    scanf("%d", &n);
+
+    int board[n];
+    solve(board, 0, n);
+
+    if (num_solutions == 0) {
+        printf("No solutions found.\n");
+    } else {
+        printf("%d solution(s) found.\n", num_solutions);
+    }
+
+    return 0;
 }
+
+bool is_safe(int board[], int row, int col, int n) {
+    for (int i = 0; i < col; i++) {
+        if (board[i] == row || board[i] == row - col + i || board[i] == row + col - i) {
+            return false;
+        }
+    }
+    return true;
+}
+void solve(int board[], int col, int n) {
+    if (col == n) {
+        print_solution(board, n);
+        num_solutions++;
+        return;
+    }
+
+    for (int row = 0; row < n; row++) {
+        if (is_safe(board, row, col, n)) {
+            board[col] = row;
+            solve(board, col+1, n);
+        }
+    }
+}
+
+void print_solution(int board[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (board[j] == i) {
+                printf("Q ");
+            } else {
+                printf(". ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
